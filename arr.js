e2d9c51367578ea5567fb86cfcd73ebb90df565c
TTS <theintheinsoe1218@gmail.com>
2026-05-19 12:54:12 +0630

    fix ui

--- arr.js
+++ arr.js
@@ -100,4 +100,101 @@ function multiplyArray(arr, num) {
     return result;
 }
 
-console.log(multiplyArray([1,2,3], 2));
+console.log(multiplyArray([1,2,3], 2)); 
+//16
+function countVowels(text) {
+    let count = 0;
+    let vowels = "aeiou";
+
+    for (let char of text.toLowerCase()) {
+        if (vowels.includes(char)) count++;
+    }
+
+    return count;
+}
+
+console.log(countVowels("Hello"));
+
+
+function login(username, password) {
+    return username === "admin" && password === "1234";
+}
+
+console.log(login("admin", "1234"));
+
+function squareArray(arr) {
+    let result = [];
+
+    for (let num of arr) {
+        result.push(num * num);
+    }
+
+    return result;
+}
+
+console.log(squareArray([2,3,4]));
+
+const user = { name: "Mg Mg", age: 20 };
+
+for (let key in user) {
+    console.log(key, user[key]);
+}
+
+
+const students = [
+    { name: "A", age: 20 },
+    { name: "B", age: 21 },
+    { name: "C", age: 22 }
+];
+
+console.log(students);
+
+
+function findStudent(students, name) {
+    for (let s of students) {
+        if (s.name === name) return s;
+    }
+    return null;
+}
+
+console.log(findStudent(students, "B"));
+
+
+function findMaxAge(users) {
+    let max = users[0].age;
+
+    for (let u of users) {
+        if (u.age > max) max = u.age;
+    }
+
+    return max;
+}
+
+const users = [
+    { name: "Aung Aung", age: 25 },
+    { name: "Mg Mg", age: 30 },
+    { name: "Su Su", age: 22 }
+];
+
+console.log(findMaxAge(users));
+
+function toggleStatus(todo) {
+    todo.isCompleted = !todo.isCompleted;
+    return todo;
+}
+const todo = { title: "Learn JavaScript", isCompleted: false };
+console.log(toggleStatus(todo));
+
+function isEqual(obj1, obj2) {
+    return JSON.stringify(obj1) === JSON.stringify(obj2);
+}
+console.log(isEqual({ name: "A", age: 20 }, { name: "A", age: 20 }));
+
+function rotateLeft(arr) {
+    let first = arr.shift();
+    arr.push(first);
+    return arr;
+}
+
+console.log(rotateLeft([1,2,3,4,5]));
+
